7d15e69bfd47320dc646332a4fa4c9f6a33dbe96
Fabio Rojas <fabio.rojas@thermofisher.com>
2023-04-02 20:45:11 -0700

    adding headers

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -6,7 +6,9 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site! Running with modd.</h1>")
+
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
