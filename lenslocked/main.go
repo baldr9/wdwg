fda79d8554c093845d174f7472296035d7dcda33
Fabio Rojas <fabio.rojas@thermofisher.com>
2023-04-03 19:06:47 -0700

    adding router

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,9 +5,22 @@ import (
 	"net/http"
 )
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site! Version: v2.0.8</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site! Version: v2.0.9</h1>")
 
 }
 
@@ -28,7 +41,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
