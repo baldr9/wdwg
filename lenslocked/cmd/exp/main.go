d6cf1a88739237500abfd123a9e32b0892b8ffda
Fabio Rojas <fabio.rojas@thermofisher.com>
2023-04-07 15:07:53 -0700

    template exercises

---
+++ lenslocked/cmd/exp/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name         string
+	JerseyNumber int
+	GamesPlayed  int
+  FavoriteFoods []string
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+  user := User{
+    Name:         "Jackie Robinson",
+    JerseyNumber: 42,
+    GamesPlayed:  33,
+    FavoriteFoods: []string{"burger", "steak", "salmon"},
+  }
+
+  err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
