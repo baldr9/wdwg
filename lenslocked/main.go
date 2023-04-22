f33bbd35e381843e3de54b00b3dcb5ec319bce7c
Fabio Rojas <fabio.rojas@thermofisher.com>
2023-04-21 22:53:31 -0700

    working with templates

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/baldr9/wdwg/lenslocked/controllers"
+	"github.com/baldr9/wdwg/lenslocked/templates"
 	"github.com/baldr9/wdwg/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 )
@@ -13,14 +13,12 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
-	r.Get("/", controllers.StaticHandler(tpl))
-
-	// Or inline everything...
+	r.Get("/", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
