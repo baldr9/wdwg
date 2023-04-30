0c6437c4bd9eb95072c04fa2d6f0c6f953a7f699
Fabio Rojas <fabio.rojas@thermofisher.com>
2023-04-30 13:06:39 -0700

    using cfg struct

--- lenslocked/cmd/exp/exp-pgx.go
+++ lenslocked/cmd/exp/exp-pgx.go
@@ -7,8 +7,33 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
 func main() {
-	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+	// Note: String() method is implemented on the PosgresConfig type
+	// cfg.String(): host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable
+	// Result is that cfg parameter key=value strings are all lowercase and separated by spaces
+	fmt.Printf("cfg.String(): %v\n", cfg.String())
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
