327001aabe68a2b1a425ae9515cf85b95b45eb9f
Fabio Rojas <fabio.rojas@thermofisher.com>
2023-05-05 20:37:29 -0700

    user service in users controller

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/baldr9/wdwg/lenslocked/controllers"
+	"github.com/baldr9/wdwg/lenslocked/models"
 	"github.com/baldr9/wdwg/lenslocked/templates"
 	"github.com/baldr9/wdwg/lenslocked/views"
 	"github.com/go-chi/chi/v5"
@@ -22,7 +23,23 @@ func main() {
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
-	var usersC controllers.Users
+	// Setup a database connection
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Setup our model services
+	userService := models.UserService{
+		DB: db,
+	}
+
+	// Setup our controllers
+	usersC := controllers.Users{
+		UserService: &userService,
+	}
 	usersC.Templates.New = views.Must(views.ParseFS(
 		templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	r.Get("/signup", usersC.New)
