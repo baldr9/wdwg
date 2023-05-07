b85e3541ba49b4b6ae08f088038bcc36c570fae1
Fabio Rojas <fabio.rojas@thermofisher.com>
2023-05-06 19:23:12 -0700

    create user in db

--- lenslocked/controllers/users.go
+++ lenslocked/controllers/users.go
@@ -23,6 +23,16 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<p>Create() Email: %s</p>", r.FormValue("email"))
-	fmt.Fprintf(w, "<p>Create() Password: %s</p>", r.FormValue("password"))
+	fmt.Fprintf(w, "<p>UserService.Create DB Email: %s</p>", r.FormValue("email"))
+	fmt.Fprintf(w, "<p>UserService.Create DB Password: %s</p>", r.FormValue("password"))
+
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User created: %+v", user)
 }
