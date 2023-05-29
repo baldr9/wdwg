5e17aca2e108c115897cab4fd186405d72837d69
Fabio Rojas <fabio.rojas@thermofisher.com>
2023-05-29 16:24:43 -0700

    create sessions

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -36,15 +36,20 @@ func main() {
 	userService := models.UserService{
 		DB: db,
 	}
+	sessionService := models.SessionService{
+		DB: db,
+	}
 
 	// Setup our controllers
 	usersC := controllers.Users{
-		UserService: &userService,
+		UserService:    &userService,
+		SessionService: &sessionService,
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(
 		templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(
 		templates.FS, "signin.gohtml", "tailwind.gohtml"))
+	// r.Get("/signup", TimerMiddleware(usersC.New))
 	r.Get("/signup", usersC.New)
 	r.Post("/signup", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
@@ -62,5 +67,15 @@ func main() {
 		// TODO: Fix this before deploying
 		csrf.Secure(false),
 	)
-	http.ListenAndServe(":3000", csrfMw(r))
+	http.ListenAndServe("localhost:3000", csrfMw(r))
 }
+
+// Uncomment the TimerMiddleware func and use it above in main() to see
+// it in action.
+// func TimerMiddleware(h http.HandlerFunc) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		start := time.Now()
+// 		h(w, r)
+// 		fmt.Println("Request time:", time.Since(start))
+// 	}
+// }
